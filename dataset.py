37406d4959c8d6a7b0993f123678ad13c396ad70
ponakilan <ponakilan25@gmail.com>
2025-03-18 07:02:53 +0000

    Dataset complete

--- dataset.py
+++ dataset.py
@@ -6,6 +6,8 @@ from torchvision import datasets
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
+from PIL import Image
+
 
 class ImageDataset(Dataset):
     def __init__(self, root_dir: str, seq_len: int = 10, max_frames: int = 200, transform=None):
@@ -15,30 +17,35 @@ class ImageDataset(Dataset):
         self.files = [list(directory) for directory in self.files]
         for i in range(len(self.files)):
             self.files[i][2] = sorted([file for file in self.files[i][2] if file[-3:] == "tif"])
-            
+
         self.seq_len = seq_len
         self.max_frames = max_frames
+        self.transform = transform
 
     def __len__(self):
         return (len(self.files) * self.max_frames) // self.seq_len
 
     def _get_files(self, index: int):
         total_frames = index * self.seq_len + self.seq_len
-
-        # Identify the directory
         dir_index = math.ceil(total_frames / self.max_frames) - 1
 
-        # Start and end indices of files in the directory
         start = (index * self.seq_len) % self.max_frames
         end = start + self.seq_len
 
         return dir_index, start, end
 
+    def _load_image(self, img_path: str):
+        image = Image.open(img_path)
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+    def _load_frames(self, base_path: str, files: str):
+        return torch.stack([self._load_image(f'{base_path}/{img_name}') for img_name in files])
+
     def __getitem__(self, index: int):
-        directory, start, end = _get_files(index)
-
-
-        return 1
-
-dataset = ImageDataset("data/UCSDped1/Train", 10)
-print(dataset._get_files(20))
+        directory, start, end = self._get_files(index)
+        base_path = self.files[directory][0]
+        files = self.files[directory][2][start:end]
+        return self._load_frames(base_path, files)
+        
