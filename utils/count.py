4181f7dff9dd6379e2881ad6ce6972d882868da8
ponakilan <ponakilan25@gmail.com>
2025-03-25 12:00:52 +0530

    Print max

--- utils/count.py
+++ utils/count.py
@@ -5,9 +5,11 @@ import sys
 def main(path: str):
     dirs = list(os.walk(path))
     min_files = 999
+    max_files = 0
     for i in range(1, len(dirs)):
         min_files = min(min_files, len(dirs[i][2]))
-    print(min_files)
+        max_files = max(max_files, len(dirs[i][2]))
+    print(min_files, max_files)
 
 if __name__ == "__main__":
     args = sys.argv
