96beae20a5a9aa8fb423401dbcb2b3261afcd9bf
ponakilan <ponakilan25@gmail.com>
2025-03-25 11:51:34 +0530

    Encode any dataset

--- encode_dataset.py
+++ encode_dataset.py
@@ -1,15 +1,29 @@
+import sys
+
 import tqdm
 import pickle
-from dataset import ImageDataset
+from dataset import EmbeddingGenerator
 
-dataset = ImageDataset(
-    root_dir="data/UCSDped1/Train",
-    seq_len=100
-)
 
-embeddings = []
-for i in tqdm.tqdm(range(len(dataset))):
-    embeddings.append(dataset[i].cpu().tolist())
+def generate_embeddings(root_dir: str, seq_len: int, save_location: str):
+    dataset = EmbeddingGenerator(
+        root_dir=root_dir,
+        seq_len=seq_len
+    )
 
-with open("embeddings_100.pkl", "wb") as f:
-    pickle.dump(embeddings, f)
+    embeddings = []
+    for i in tqdm.tqdm(range(len(dataset))):
+        embeddings.append(dataset[i].cpu().tolist())
+
+    with open(save_location, "wb") as f:
+        pickle.dump(embeddings, f)
+    
+    print(f"{len(embeddings)} embeddings saved to {save_location}.")
+
+
+if __name__ == "__main__":
+    root_dir = sys.argv[1]
+    seq_len = sys.argv[2]
+    save_location = sys.argv[3]
+
+    generate_embeddings(root_dir, seq_len, save_location)
